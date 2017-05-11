a2204e69fcd97bf6a5e468a2985c7d009e57fde8
Matthew Wells <matthew@ergon.tech>
2017-05-11 15:34:35 -0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# ErgonTech_SimpleProductAlerts
+TODO: Add introductory description.
+
+## Installation
+
+TODO: Add repository details, if necessary.
+
+TODO: Update version constraint, if applicable
+
+Require the module by running the following command:
+```bash
+composer require ergontech/simpleproductalerts:dev-master
+```
+
+## Usage
+
+TODO: Add usage details.
---
+++ test/app/code/community/ErgonTech/SimpleProductAlert/etc/ConfigTest.php
@@ -0,0 +1,17 @@
+<?php
+class ErgonTech_SimpleProductAlerts_etc_ConfigTest extends \MageTest_PHPUnit_Framework_TestCase
+{
+
+protected $config;
+    public function setUp()
+    {
+        $this->config = new \Mage_Core_Model_Config_Base(__DIR__ . '/../../../../../../../app/code/community/ErgonTech/SimpleProductAlert/etc/config.xml');
+    }
+    public function testModuleDeclaration()
+    {
+        $module = $this->config->getNode('modules/ErgonTech_SimpleProductAlert');
+        
+        static::assertEquals('ErgonTech_SimpleProductAlert', $module->getName());
+        
+        static::assertTrue(version_compare($module->version, '0.0.0', '>='));
+    }}
