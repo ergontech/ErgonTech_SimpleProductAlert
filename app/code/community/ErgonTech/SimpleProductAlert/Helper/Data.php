4c2bd0bc9097e0458a7f93cb3eb3c0ccd7535917
Matthew Wells <matthew@ergon.tech>
2017-11-19 18:56:57 -0800

    allow generation of a configurable attribute data attr string
     - to make each notify link identifiable

--- app/code/community/ErgonTech/SimpleProductAlert/Helper/Data.php
+++ app/code/community/ErgonTech/SimpleProductAlert/Helper/Data.php
@@ -17,4 +17,20 @@ class ErgonTech_SimpleProductAlert_Helper_Data extends Mage_Core_Helper_Abstract
 
         return $callable;
     }
+
+    public function generateConfigurableAttributesAttrs(
+        Mage_Catalog_Model_Resource_Product_Type_Configurable_Attribute_Collection $configurableAttributes,
+        Mage_Catalog_Model_Product $child
+    ) {
+        $out = [];
+        foreach ($configurableAttributes as $configurableAttribute) {
+            /** @var Mage_Eav_Model_Entity_Attribute $productAttribute */
+            $productAttribute = $configurableAttribute->getProductAttribute();
+            $out[] = sprintf('data-%d="%s"',
+                $productAttribute->getId(),
+                $child->getData($productAttribute->getAttributeCode()));
+        }
+
+        return implode(' ', $out);
+    }
 }
