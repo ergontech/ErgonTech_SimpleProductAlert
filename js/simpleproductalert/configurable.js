ff92bf2cdec294a8b65eeb7a4796f4b81de4612f
Matthew Wells <matthew@ergon.tech>
2017-11-27 18:23:02 -0800

    NWBIS-4: hide/show add-to-cart button to prevent OoS A2C attempts

--- js/simpleproductalert/configurable.js
+++ js/simpleproductalert/configurable.js
@@ -2,6 +2,10 @@ if (Product instanceof Object &&
     Product.Config instanceof Object
 ) {
     var configProto = Product.Config.prototype;
+    var cartButton;
+    document.observe('dom:loaded', function () {
+       cartButton = document.querySelector('#product_addtocart_form [onclick*="productAddToCartForm.submit"]');
+    });
 
     configProto.fillSelect = configProto.fillSelect.wrap(function (_super, element) {
         _super(element);
@@ -9,16 +13,20 @@ if (Product instanceof Object &&
         var disabledOptions = element.querySelectorAll('option:disabled');
 
         Array.prototype.slice.call(disabledOptions).forEach(function (disabledOption) {
-            disabledOption.setAttribute('data-allow-notify', true);
+            disabledOption.setAttribute('data-out-of-stock', true);
             disabledOption.disabled = false;
         });
     });
 
     configProto.configure = configProto.configure.wrap(function (_super, ev) {
         $$('.simpleproductalert_notify-link').invoke('hide');
+        cartButton.style.display = 'initial';
         _super(ev);
 
         var notifySelector = this.settings.map(function (el) {
+            if (el.options[el.selectedIndex].getAttribute('data-out-of-stock')) {
+                cartButton.style.display = 'none';
+            }
             return '[' + el.id.replace('attribute', 'data-') + '=' + el.value + ']'
         }).join('');
 
