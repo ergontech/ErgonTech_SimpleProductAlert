c40668d8c709cdaa1cb2cd393edf5a9f62400580
Matthew Wells <matthew@ergon.tech>
2017-12-13 11:51:21 -0800

    Use a const to indicate what "2" means

--- app/code/community/ErgonTech/SimpleProductAlert/Helper/Productalert/Data.php
+++ app/code/community/ErgonTech/SimpleProductAlert/Helper/Productalert/Data.php
@@ -2,6 +2,8 @@
 
 class ErgonTech_SimpleProductAlert_Helper_Productalert_Data extends Mage_ProductAlert_Helper_Data
 {
+    const PRODUCTALERT_STOCK_USE_CONFIG = 2;
+
     /**
      * Allow products to override the enabledness of product stock alerts
      *
@@ -11,7 +13,7 @@ class ErgonTech_SimpleProductAlert_Helper_Productalert_Data extends Mage_Product
     {
         $allowed = $this->getProduct()->getData('productalert_stock');
 
-        if ($allowed === null || $allowed == 2) {
+        if ($allowed === null || $allowed == self::PRODUCTALERT_STOCK_USE_CONFIG) {
             return parent::isStockAlertAllowed();
         }
 
